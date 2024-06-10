d9d6010c91c3794d982aaaa04116a4bca202afc9
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-06-10 20:29:26 +0100

    Added function for showing timezone

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-alert("Hello World");
+function displayTimezone(event) {
+  let parisTime = moment()
+    .tz("Europe/Paris")
+    .format("dddd, MMMM D, YYYY h:mm A");
+  let tokyoTime = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:mm A");
+  let sydneyTime = moment()
+    .tz("Australia/Sydney")
+    .format("dddd, MMMM D, YYYY h:mm A");
+
+  if (event.target.value == "Paris") {
+    alert(`It is ${parisTime} in Europe/Paris`);
+  }
+
+  if (event.target.value == "Tokyo") {
+    alert(`It is ${tokyoTime} in Asia/Tokyo`);
+  }
+
+  if (event.target.value == "Sydney") {
+    alert(`It is ${sydneyTime} in Australia/Sydney`);
+  }
+}
+
+let showTime = document.querySelector("#country");
+showTime.addEventListener("change", displayTimezone);
